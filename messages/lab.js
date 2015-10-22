e24d1f6bb73176f11eb7ab8b9745e0f8d942aabc
rohundhar <rohundhar@gmail.com>
2015-10-21 20:18:49 -0400

    update

--- messages/lab.js
+++ messages/lab.js
@@ -8,10 +8,10 @@ request.onreadystatechange = function() {
 	if (request.readyState == 4){
 		console.log("DATA RECEIVED!");
 	}
-
+	request.send(null);
+	data = request.responseText;
 }
-request.send(null);
-data = request.responseText;
+
 console.log("this is the data:", data);
 parsedData = JSON.parse(data);
 
