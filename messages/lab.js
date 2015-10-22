3b5d22b72c2488be9274dc4ad305ee792cdf1637
rohundhar <rohundhar@gmail.com>
2015-10-21 22:54:24 -0400

    update6

--- messages/lab.js
+++ messages/lab.js
@@ -2,18 +2,21 @@ function parse(){
 var data;
 var parsedData;
 var request = new XMLHttpRequest();
-request.open("GET", "data.json", true);
 
-/*request.onreadystatechange = function() {
+//request.open("GET", "data.json", true);
+
+request.onreadystatechange = function() {
 	if (request.readyState == 4){
 		console.log("DATA RECEIVED!");
+		var parsedData = JSON.parse(request.responseText);
+		console.log("parsed the data", parsedData);
 	} */
-	request.send(null);
-	data = request.responseText;
-//}
+	//request.send(null);
+}
+request.open("GET", "data.json", true);
+request.send(null);
 
-
-parsedData = JSON.parse("data.json");
-console.log("this is the data:", parsedData);
+//parsedData = JSON.parse(data);
+//console.log("this is the data:", parsedData);
 
 }
